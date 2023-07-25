bdec4156ed8f9d36f7577034c827e2d3878d0da5
sirineao <sirineaoudj2@gmail.com>
2023-07-24 21:55:23 -0400

    added the method to sort the TopN and api route for TopN

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, render_template, jsonify
+from db_util import *
+
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
 
-@app.route('/')
-def index():
-    return "hello, world!"
+def list_to_json(topN):
+    this_list = []
+    for url, score in topN:
+       x = {
+        "url" : url,
+        "score": score
+       }
+       this_list.append(x)
+    
+    return jsonify(this_list)
+
+def sortTopN(n, data):
+    topN = sorted(data, key = lambda x: x[1], reverse = True)[:int(n)]
+    return topN
+
+@app.route('/top/<n>')
+def get_n(n):
+    data = query_from_db()
+    topN = sortTopN(n,data)
+    return list_to_json(topN)
 
 if __name__ == "__main__":
     app.run(debug=True)
